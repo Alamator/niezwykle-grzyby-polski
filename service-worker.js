ea38d9ee7c6d67ccf66c3b49a2c1cee90ac9cba4
Alamator <janusz.alamator@gmail.com>
2026-05-08 17:40:25 +0200

    Bump service worker cache to v0.3

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,48 @@
-const CACHE_NAME = "niezwykle-grzyby-polski-v0-2-0";
+const CACHE_NAME = "niezwykle-grzyby-polski-v0-3-0";
 const ASSETS = [
   "./",
   "./index.html",
   "./css/styles.css",
   "./js/app.js",
   "./data/mushrooms.js",
+  "./data/photo-pack-v03.js",
   "./assets/favicon.svg",
   "./assets/logo-oroloko-ai.svg",
   "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+
+  const requestUrl = new URL(event.request.url);
+  const isLocalAsset = requestUrl.origin === self.location.origin;
+
+  if (!isLocalAsset) {
+    event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
